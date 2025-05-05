9e48f4b2f1318c7aa95c781b03c483838cdb814a
nosorrow <plamenorama@gmail.com>
2025-05-05 20:06:43 +0300

    Add Sidebar+content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { Badge, Button } from "flowbite-react";
+import { Badge, Button, cardTheme } from "flowbite-react";
 import {
   Sidebar,
   SidebarItem,
@@ -16,8 +16,8 @@ import {
   HiUser,
   HiViewBoards,
 } from "react-icons/hi";
-import { RiFolder3Fill } from "react-icons/ri";
-
+import { CiFolderOn } from "react-icons/ci";
+import "flowbite";
 
 function App() {
   const [categories, setCategories] = useState([]);
@@ -69,113 +69,126 @@ function App() {
   );
 
   return (
-    <div className="flex gap-10">
-      <Sidebar aria-label="Default sidebar example" className="h-screen">
-        <SidebarItems>
-          <SidebarItemGroup>
-                      
-          {categories.map((category) => (
-            
-              <SidebarItem 
-                href="#"
-                key={category.name}
-                onClick={() => loadIcons(category.name)}
-                className={`${
-                  category.name === selectedCategory
-                    ? "bg-blue-600 text-white"
-                    : ""
-                }`}
-              >
-                {category.name} {/* Added category name display */}
-              </SidebarItem>
+    <div className="w-full">
+      <button
+        data-drawer-target="default-sidebar"
+        data-drawer-toggle="default-sidebar"
+        aria-controls="default-sidebar"
+        type="button"
+        className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+      >
+        <span className="sr-only">Open sidebar</span>
+        <svg
+          className="w-6 h-6"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            clip-rule="evenodd"
+            fill-rule="evenodd"
+            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+          ></path>
+        </svg>
+      </button>
+      <aside
+        id="default-sidebar"
+        className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+          <ul className="space-y-2 font-medium">
+            {categories.map((category) => (
+              <>
+                <li>
+                  <a
+                    href="#"
+                    key={category.name}
+                    onClick={() => loadIcons(category.name)}
+                    className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group
+                     ${
+                       category.name === selectedCategory
+                         ? "bg-blue-600 text-white"
+                         : ""
+                     }`}
+                  >
+                    <CiFolderOn />
+                    <span className="flex-1 ms-3 whitespace-nowrap">
+                      { category.name}
+                    </span>
+                    <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
+                      {category.count}
+                    </span>
+                  </a>
+                </li>
+              </>
             ))}
-
-            <SidebarItem href="#" icon={HiChartPie}>
-              Dashboard
-            </SidebarItem>
-            <SidebarItem
-              href="#"
-              icon={HiViewBoards}
-              label="Pro"
-              labelColor="dark"
-            >
-              Kanban
-            </SidebarItem>
-            <SidebarItem href="#" icon={HiInbox} label="3">
-              Inbox
-            </SidebarItem>
-            <SidebarItem href="#" icon={HiUser}>
-              Users
-            </SidebarItem>
-            <SidebarItem href="#" icon={HiShoppingBag}>
-              Products
-            </SidebarItem>
-            <SidebarItem href="#" icon={HiArrowSmRight}>
-              Sign In
-            </SidebarItem>
-            <SidebarItem href="#" icon={HiTable}>
-              Sign Up
-            </SidebarItem>
-          </SidebarItemGroup>
-        </SidebarItems>
-      </Sidebar>
-      <div className="w-full mb-6">
-        <h1 className="text-3xl font-bold mb-6 text-center">Icon Browser</h1>
-        <div className="flex flex-wrap justify-center gap-2 mb-6">
-          {categories.map((category) => (
-            <button
-              key={category.name}
-              onClick={() => loadIcons(category.name)}
-              className={`px-4 py-2 rounded ${
-                category.name === selectedCategory
-                  ? "bg-blue-600 text-white"
-                  : "bg-gray-200 hover:bg-gray-300"
-              }`}
-            >
-              {category.name}
-              <Badge
-                color="purple"
-                className="ml-2 inline-flex"
-                size="xs"
-                style={{ fontSize: "0.75rem" }}
-              >
-                {category.count}
-              </Badge>
-            </button>
-          ))}
+          </ul>
         </div>
+      </aside>
 
-        {selectedCategory && (
-          <div className="flex justify-center mb-6">
-            <input
-              type="text"
-              placeholder="Търси икона..."
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              className="input input-bordered w-full max-w-md px-4 py-2 border rounded-lg"
-            />
-          </div>
-        )}
-
-        {loading && <div className="text-center text-xl">Зареждане...</div>}
-
-        <div className="grid grid-cols-2 md:grid-cols-8 gap-6 mt-12">
-          {filteredIcons.map((icon) => (
-            <div key={icon.name} className="py-4 flex flex-col items-center">
-              <div
-                dangerouslySetInnerHTML={{ __html: icon.svg }}
-                className="icon"
-              />
-              <div className="text-sm mt-2 text-center">{icon.name}</div>
-              <Button
-                color="alternative"
-                onClick={() => copyToClipboard(icon.svg)}
-                className="mt-2"
-              >
-                Copy
-              </Button>
+      <div className="p-4 sm:ml-64">
+        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+        <div className="grid grid-cols-3 gap-4 mb-4">
+            <div className="flex items-center justify-center h-24 rounded-sm bg-gray-50 dark:bg-gray-800">
+              <p className="text-2xl text-gray-400 dark:text-gray-500">
+                <svg
+                  className="w-3.5 h-3.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 18 18"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 1v16M1 9h16"
+                  />
+                </svg>
+              </p>
             </div>
-          ))}
+            <div className="flex items-center justify-center h-24 rounded-sm bg-gray-50 dark:bg-gray-800">
+              <p className="text-2xl text-gray-400 dark:text-gray-500">
+                <svg
+                  className="w-3.5 h-3.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 18 18"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 1v16M1 9h16"
+                  />
+                </svg>
+              </p>
+            </div>
+            <div className="flex items-center justify-center h-24 rounded-sm bg-gray-50 dark:bg-gray-800">
+              <p className="text-2xl text-gray-400 dark:text-gray-500">
+                <svg
+                  className="w-3.5 h-3.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 18 18"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 1v16M1 9h16"
+                  />
+                </svg>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
