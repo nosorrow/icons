121597d33dd4012479bf6c2faf2b244caa00392a
nosorrow <plamenorama@gmail.com>
2025-05-06 22:09:16 +0300

    fix border + add open folder on sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import SidebarLogo from "./components/sidebar-logo";
 import { Badge, Button, cardTheme } from "flowbite-react";
-import { CiFolderOn } from "react-icons/ci";
+import { FaRegFolder, FaRegFolderOpen } from "react-icons/fa";
 import "flowbite";
 
 function App() {
@@ -81,7 +81,7 @@ function App() {
       </button>
       <aside
         id="default-sidebar"
-        className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+        className="fixed top-0 left-0 z-40 w-72 h-screen transition-transform -translate-x-full sm:translate-x-0"
         aria-label="Sidebar"
       >
         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
@@ -99,15 +99,16 @@ function App() {
                   className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group
                      ${
                        category.name === selectedCategory
-                         ? "bg-red-50 text-black border-2 border-red-400"
+                         ? "bg-red-50 text-black ring-2 ring-red-400"
                          : ""
                      }`}
                 >
-                  <CiFolderOn />
+                 { category.name === selectedCategory ? (<FaRegFolderOpen />): (<FaRegFolder />) }
+                 
                   <span className="flex-1 ms-3 whitespace-nowrap">
                     {category.name}
                   </span>
-                  <span className="inline-flex items-center justify-center w-8 h-8 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
+                  <span className="inline-flex items-center justify-center min-w-3 h-3 p-3 ms-3 text-sm font-medium text-indigo-800 bg-indigo-100 rounded-full dark:bg-indigo-900 dark:text-indigo-300">
                     {category.count}
                   </span>
                 </a>
@@ -117,7 +118,7 @@ function App() {
         </div>
       </aside>
 
-      <div className="p-4 sm:ml-64">
+      <div className="p-4 sm:ml-72">
         <div>
           {selectedCategory && (
             <div className="flex justify-center mb-6">
